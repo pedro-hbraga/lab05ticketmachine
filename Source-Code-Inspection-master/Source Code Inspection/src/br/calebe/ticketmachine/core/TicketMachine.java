3ece00c761b6acb31cfdbd2a9ea5a6bdb1c7ffd9
Gustavo Versolato <versolatogustavo@gmail.com>
2023-09-23 15:33:02 -0300

    Correção no for da classe Troco e impressão da classe ticketmachine

--- Source-Code-Inspection-master/Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source-Code-Inspection-master/Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -22,7 +22,7 @@ public class TicketMachine {
     public void inserir(int quantia) throws PapelMoedaInvalidaException {
         boolean achou = false;
         for (int i = 0; i < papelMoeda.length && !achou; i++) {
-            if (papelMoeda[1] == quantia) {
+            if (papelMoeda[i] == quantia) {
                 achou = true;
             }
         }
@@ -45,7 +45,7 @@ public class TicketMachine {
             throw new SaldoInsuficienteException();
         }
         String result = "*****************\n";
-        result += "*** R$ " + saldo + ",00 ****\n";
+        result += "*** R$ " + valor + ",00 ****\n";
         result += "*****************\n";
         return result;
     }
